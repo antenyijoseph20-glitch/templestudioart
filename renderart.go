71cfbdd165147dc5914c70b4d84ebc58eb241394
antenyijoseph20-glitch <antenyijoseph20@gmail.com>
2026-07-07 23:02:21 +0100

    Add renderArt function for ASCII art rendering
    Implement renderArt function to process input and load banner.

---
+++ renderart.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	// //"fmt"
+)
+
+func renderArt(input string, bannerFile string) (string, error) {
+
+	input = strings.ReplaceAll(input, "\r\n", "\n")
+	input = strings.ReplaceAll(input, "\r", "\n")
+	// Is the input valid?
+	_, err := validateArt(input)
+	if err != nil {
+		return "", err
+	}
+	// Can we load the banner?
+	banner, err := loadBanner(bannerFile)
+	if err != nil {
+		return "", err
+	}
+	// Build the ASCII art.
+	result := generateArt(input, banner)
+
+	return result, nil
+
+}
